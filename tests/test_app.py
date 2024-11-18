903332b39f10bef099e0394f026f071f3d034910
zak18 <zak18@mail.aub.edu>
2024-11-18 03:49:01 +0200

    Fix test discovery for unittest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,11 @@
 import unittest
 from app import greet
 
+
 class TestApp(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello, World!")
+
 
 if __name__ == "__main__":
     unittest.main()
